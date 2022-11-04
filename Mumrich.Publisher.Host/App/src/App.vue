cc118c85dd5a6a145c479e44372a754a7c49676e
Hannes Morgenthaler <hanneszbinden@outlook.com>
2022-11-04 21:15:11 +0100

    add vuetify 3.

--- Mumrich.Publisher.Host/App/src/App.vue
+++ Mumrich.Publisher.Host/App/src/App.vue
@@ -1,5 +1,23 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div>test</div>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const drawer = ref<boolean | null>(null);
+</script>
